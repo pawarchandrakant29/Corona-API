b962aef74e4f366e336bd75f768472f0b8f5bd5d
pawarchandrakant29 <chandrakantpawar590@gmail.com>
2024-03-19 21:03:36 +0530

    update

--- js/script.js
+++ js/script.js
@@ -11,13 +11,14 @@ fetch('https://data.covid19india.org/data.json')
             coronacases.innerHTML += `
                 <tr>
                     <td class="border border-white">${details.state}</td>
-                    <td class="border border-white">${details.active}</td>
+                    <td class="border border-white text-warning fw-semibold">${details.active}</td>
                     <td class="border border-white">${details.lastupdatedtime}</td>
-                    <td class="border border-white">${details.confirmed}</td>
-                    <td class="border border-white">${details.deaths}</td>
+                    <td class="border border-white text-success fw-semibold">${details.confirmed}</td>
+                    <td class="border border-white text-danger fw-semibold">${details.deaths}</td>
                 </tr>`;
         });
     })
     .catch((err) => {
-        console.log("error data not found",err);
+        console.log("error data not found", err);
     });
+
