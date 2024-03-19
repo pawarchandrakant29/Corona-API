0257ed4bbae78767905bf1d4d51aa3d9ac16801f
pawarchandrakant29 <chandrakantpawar590@gmail.com>
2024-03-19 20:15:32 +0530

    upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let coronacases = document.getElementById('coronacase');
+
+fetch('https://data.covid19india.org/data.json')
+    .then((res) => {
+        return res.json();
+    })
+    .then((data) => {
+        const statedata = data.statewise;
+
+        statedata.forEach((details) => {
+            coronacases.innerHTML += `
+                <tr>
+                    <td class="border border-white">${details.state}</td>
+                    <td class="border border-white">${details.active}</td>
+                    <td class="border border-white">${details.lastupdatedtime}</td>
+                    <td class="border border-white">${details.confirmed}</td>
+                    <td class="border border-white">${details.deaths}</td>
+                </tr>`;
+        });
+    })
+    .catch((err) => {
+        console.log("error data not found",err);
+    });
